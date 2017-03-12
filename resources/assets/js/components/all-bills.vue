251ff74c8aef1a7884176ea3bd4e44acf4c036b6
Austin Lord <ahlord@iu.edu>
2017-03-12 12:29:46 -0400

    rework search UI
    - filter when the form is submitted, rather than when the input changes
    - display message indicating results shown

--- resources/assets/js/components/all-bills.vue
+++ resources/assets/js/components/all-bills.vue
@@ -3,43 +3,74 @@
         <h1 class="section-title">All bills</h1>
         <div>
             <div class="filters">
-                <input class="filters__search" type="search" autocomplete="off" v-model="q" placeholder="Filter bills">
+                <form class="filters__search search" @submit.prevent="filterBillHandler">
+                    <input class="search__input" type="search" autocomplete="off" v-model="q" placeholder="Filter bills">
+                    <input class="search__submit" type="submit" value="Filter bills">
+                </form>
+
+                <div class="filters__message" v-if="filteredBills.length">
+                    <div v-if="isFilterApplied">
+                        Here {{filteredBills.length | pluralizeIs}} the {{filteredBills.length}} {{filteredBills.length | pluralizeBill}} that match your search. <button class="button--plain" @click.prevent="clearSearch">Clear search</button>
+                    </div>
+                    <div v-else>
+                        Here are all {{filteredBills.length}} bills
+                    </div>
+                </div>
             </div>
             <bill-list v-if="filteredBills.length" :bills="filteredBills"></bill-list>
 
-            <div v-if="this.q.length>0 && filteredBills.length==0">
-                Your search did not return any results.
+            <div class="filters__no-result" v-if="isFilterApplied && !filteredBills.length">
+                Your search did not return any results.  <button class="button--plain" @click.prevent="clearSearch">Clear search</button>
             </div>
         </div>
     </div>
 </template>
 
-<script>
+<script type="text/babel">
     const moment = require('moment');
 
     module.exports = {
         components: {
             billList: require('./bill-list.vue'),
         },
-        computed: {
-            filteredBills() {
+        filters: {
+            pluralizeIs(value) {
+                return value == 1 ? "is" : "are";
+            },
+            pluralizeBill(value) {
+                return value == 1 ? "bill" : "bills";
+            }
+        },
+        methods: {
+            getFilteredBills() {
                 if(this.q.length > 0) {
+                    this.isFilterApplied = true
                     return this.bills.filter( bill => {
                         let subjects = bill.subjects.reduce( (acc, subject) => acc+' '+subject.Name, '')
                         return bill.Title.toLowerCase().indexOf(this.q)!==-1
-                            || bill.Name.toLowerCase().indexOf(this.q)!==-1
-                            || bill.Description.toLowerCase().indexOf(this.q)!==-1
-                            || (bill.subjects.length && subjects.toLowerCase().indexOf(this.q)!==-1)
+                                || bill.Name.toLowerCase().indexOf(this.q)!==-1
+                                || bill.Description.toLowerCase().indexOf(this.q)!==-1
+                                || (bill.subjects.length && subjects.toLowerCase().indexOf(this.q)!==-1)
                     })
                 } else {
+                    this.isFilterApplied = false
                     return this.bills
                 }
+            },
+            filterBillHandler() {
+                this.filteredBills = this.getFilteredBills();
+            },
+            clearSearch() {
+                this.q = "";
+                this.filteredBills = this.bills;
+                this.isFilterApplied = false;
             }
         },
         mounted() {
             // load all bills
             this.$http.get('/api/bills').then(res => {
                 this.bills = res.body;
+                this.filteredBills = this.getFilteredBills();
             }, res => {
                 console.log(res)
             })
@@ -47,7 +78,9 @@
         data() {
             return {
                 q: '',
+                isFilterApplied: false,
                 bills: [],
+                filteredBills: [],
                 currentSession: moment().year()
             }
         }
--- resources/assets/js/components/my-bills.vue
+++ resources/assets/js/components/my-bills.vue
@@ -2,42 +2,74 @@
     <div>
         <h1 class="section-title">My bills</h1>
         <div class="filters">
-            <input class="filters__search" type="search" autocomplete="off" v-model="q" placeholder="Filter bills">
+            <form class="filters__search search" @submit.prevent="filterBillHandler">
+                <input class="search__input" type="search" autocomplete="off" v-model="q" placeholder="Filter bills">
+                <input class="search__submit" type="submit" value="Filter bills">
+            </form>
+
+            <div class="filters__message" v-if="filteredBills.length">
+                <div v-if="isFilterApplied">
+                    {{filteredBills.length}} {{filteredBills.length | pluralizeBill}} you're tracking matched your search.  <button class="button--plain" @click.prevent="clearSearch">Clear search</button>
+                </div>
+                <div v-else>
+                    Here {{filteredBills.length | pluralizeIs}} {{filteredBills.length | pluralizeAll}} {{filteredBills.length}} {{filteredBills.length | pluralizeBill}} you're tracking
+                </div>
+            </div>
         </div>
-        <div v-if="filteredBills.length">
-            <p>Here are the {{filteredBills.length}} bills you're tracking for the <strong>{{currentSession}}</strong> session</p>
-            <bill-table :bills="filteredBills"></bill-table>
-        </div>
-        <div v-if="this.q.length>0 && filteredBills.length==0">
-            Your search did not return any results.
+        <bill-table v-if="filteredBills.length" :bills="filteredBills"></bill-table>
+
+        <div v-if="isFilterApplied && !filteredBills.length" class="filters__no-result">
+            Your search did not return any results.  <button class="button--plain" @click.prevent="clearSearch">Clear search</button>
         </div>
     </div>
 </template>
 
-<script>
+<script type="text/babel">
     const moment = require('moment');
 
     module.exports = {
         components: {
             billTable: require('./bill-table.vue'),
         },
-        computed: {
-            filteredBills() {
+        filters: {
+            pluralizeIs(value) {
+                return value == 1 ? "is" : "are";
+            },
+            pluralizeBill(value) {
+                return value == 1 ? "bill" : "bills";
+            },
+            pluralizeAll(value) {
+                return value == 1 ? "the" : "all";
+            }
+        },
+        methods: {
+            getFilteredBills() {
                 if(this.q.length > 0) {
+                    this.isFilterApplied = true
                     return this.bills.filter( bill => {
                         return bill.Title.toLowerCase().indexOf(this.q)!==-1
                                 || bill.Name.toLowerCase().indexOf(this.q)!==-1
 
                     })
                 } else {
+                    this.isFilterApplied = false
                     return this.bills
                 }
+            },
+            filterBillHandler() {
+                this.filteredBills = this.getFilteredBills();
+            },
+            clearSearch() {
+                this.q = "";
+                this.filteredBills = this.bills;
+                this.isFilterApplied = false;
             }
         },
         mounted() {
             // load all bills
             this.$http.get('/api/my-bills').then(res => {
                 this.bills = res.body;
+                this.filteredBills = this.getFilteredBills();
             }, res => {
                 console.log(res)
             })
@@ -45,7 +77,9 @@
         data() {
             return {
                 q: '',
+                isFilterApplied: false,
                 bills: [],
+                filteredBills: [],
                 currentSession: moment().year()
             }
         }
