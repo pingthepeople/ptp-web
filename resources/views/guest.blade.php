79ae39df5a6aa7ca4e0c83bfc43c17783c5ae33c
Austin Lord <ahlord@iu.edu>
2017-03-17 14:50:35 -0400

    add login with google button

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -6,6 +6,8 @@
     <div class="guest-welcome">
         <h1>Welcome to <span class="logo">Ping the People</span></h1>
 
-        <a class="button button--fb" href="{{url('/login-via-facebook')}}">Login with facebook</a>
+        <a class="button button--fb" href="{{url('/login-via-facebook')}}">Login with Facebook</a>
+
+        <a class="button button--google" href="{{url('/login-via-google')}}">Login with Google</a>
     </div>
 @endsection
