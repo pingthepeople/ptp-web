f6b556ba5cbc01ea3499b6c63a68e647bf221e73
John Hoerr <jhoerr@iu.edu>
2017-03-18 13:29:45 -0400

    Edit verbiage on account page

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -23,17 +23,18 @@
                     <label for="name">Name</label>
                     <input type="text" value="{{$user->Name}}" id="name" name="Name">
 
-                    <label for="email">Email</label>
+                    <label for="email">Email Address for Alerts</label>
                     <input type="email" value="{{$user->Email}}" id="email" name="Email">
 
-                    <label for="mobile">Mobile number</label>
+                    <label for="mobile">Mobile Number for Alerts</label>
                     <input type="tel" value="{{$user->Mobile}}" id="mobile" name="Mobile">
 
                     <fieldset>
-                        <legend>Daily email digest</legend>
-                        <label for="none"><input type="radio" name="DigestType" id="none" value="0" {{$user->DigestType==0 ? 'checked' : ''}}> None</label>
-                        <label for="myBills"><input type="radio" name="DigestType" id="myBills" value="1" {{$user->DigestType==1 ? 'checked' : ''}}> My Bills</label>
-                        <label for="allBills"><input type="radio" name="DigestType" id="allBills" value="2" {{$user->DigestType==2 ? 'checked' : ''}}> All Bills</label>
+                        <legend>Daily legislative activity digest</legend>
+                        <p>Every weekday evening during the legislative session we can send you an email with a brief roundup of that day's legislative activity. You can choose to get updates on all bill activity, or just the bills that you're watching.</p>
+                        <label for="none"><input type="radio" name="DigestType" id="none" value="0" {{$user->DigestType==0 ? 'checked' : ''}}> I do not wish to receive this.</label>
+                        <label for="myBills"><input type="radio" name="DigestType" id="myBills" value="1" {{$user->DigestType==1 ? 'checked' : ''}}> Send me a digest of the bills I am watching.</label>
+                        <label for="allBills"><input type="radio" name="DigestType" id="allBills" value="2" {{$user->DigestType==2 ? 'checked' : ''}}> Send me a digest of all bills.</label>
                     </fieldset>
 
                     <div class="account__submit">
