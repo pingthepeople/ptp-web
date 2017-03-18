937127b2a2eccee9d4051ef8f756d5cfef0a8352
Austin Lord <ahlord@iu.edu>
2017-03-17 22:35:40 -0400

    tweak login button styles

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -6,8 +6,8 @@
     <div class="guest-welcome">
         <h1>Welcome to <span class="logo">Ping the People</span></h1>
 
-        <a class="button button--fb" href="{{url('/login-via-facebook')}}">Login with Facebook</a>
+        <a class="button button--fb" href="{{url('/login-via-facebook')}}">Log in with Facebook</a>
 
-        <a class="button button--google" href="{{url('/login-via-google')}}">Login with Google</a>
+        <a class="button button--google" href="{{url('/login-via-google')}}">Log in with Google</a>
     </div>
 @endsection
