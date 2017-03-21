f58db0980ca704f9157eb59dde3d616785489e03
Austin Lord <ahlord@iu.edu>
2017-03-20 21:40:21 -0400

    utilize generateBillReport API for .csv generation

--- app/Http/Controllers/SpreadsheetExportController.php
+++ app/Http/Controllers/SpreadsheetExportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Bill;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use League\Csv\Writer;
@@ -15,14 +16,19 @@ class SpreadsheetExportController extends Controller
         if(!$user) {
             return response('', 403);
         }
-        $bills = $user->bills;
 
         $csv = Writer::createFromFileObject(new \SplTempFileObject());
 
-        $csv->insertOne(['Bill', 'Title', 'Description', 'Authors', 'Origin Chamber', 'Subjects']);
+        $client = new Client(['timeout'=>30]);
+        $response = $client->request('POST', env('BILL_REPORT_URL'), ['json'=>[
+            'Id' => $user->id,
+            'Secret' => env('BILL_REPORT_SECRET')
+        ]]);
+        $bills = json_decode($response->getBody(), true);
 
+        $csv->insertOne(array_keys($bills[0]));
         foreach ($bills as $bill) {
-            $csv->insertOne($bill->toRowArray());
+            $csv->insertOne($bill);
         }
 
         $csv->output('bills.csv');
