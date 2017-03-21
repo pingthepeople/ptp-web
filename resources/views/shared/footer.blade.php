d8f794e907a532f093215f946446282190902e36
John Hoerr <jhoerr@iu.edu>
2017-03-21 10:29:53 -0400

    Add simple footer with copyright and contact info

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <p>&copy; 2017 <a href="https://pingthepeople.org">Ping The People</a>. <span style="margin-left:1em;">Questions/Comments/Bugs? <a href="mailto:help@pingthepeople.org">Get in touch!</a></span><p>
+            </div>
+        </div>
+    </div>
+</footer>
