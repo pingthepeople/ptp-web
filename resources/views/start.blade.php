fb8b74fe92705292f2b53b322931827e085e70b0
John Hoerr <jhoerr@iu.edu>
2017-03-21 09:08:00 -0400

    Tweak verbiage tweaks

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="bill-list bill-list--all-bills">
         <h1>Get started</h1>
-        <p>You are net yet watching any bills this session. Search for bills and click the "Watch" button to get started. Be sure to review your <a href="/account">contact preferences</a> if you'd like to receive alerts about the bills you're watching.</p>
+        <p>You are net yet watching any bills this session. Search for bills and click the "Watch" button to add them to your list. Be sure to review your <a href="/account">contact preferences</a> if you'd like to receive alerts about the bills you're watching.</p>
 
         <div class="bill-list bill-list--all-bills">
             <all-bills></all-bills>
