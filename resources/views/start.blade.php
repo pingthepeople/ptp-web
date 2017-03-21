66cd2dc98e4c1a8ca8628ffba042a25b996e4c6b
John Hoerr <jhoerr@iu.edu>
2017-03-21 08:37:09 -0400

    Tweak start blade verbiage

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="bill-list bill-list--all-bills">
         <h1>Get started</h1>
-        <p>You are net yet tracking any bills this session.</p>
+        <p>You are net yet watching any bills this session. Search for bills and click the "Watch" button to get started. Be sure to review your <a href="/account">contact preferences</a> if you'd like to receive alerts about the bills you're watching.</p>
 
         <div class="bill-list bill-list--all-bills">
             <all-bills></all-bills>
