c61555e56b8527d9db5528b4ee21ff969c6f63c8
Austin Lord <ahlord@iu.edu>
2017-03-26 20:48:48 -0400

    change em to strong for consistency

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Ping the People')
 
 @section('content')
-    <h1>Welcome to <em>Ping the People</em>!</h1>
+    <h1>Welcome to <strong>Ping the People</strong>!</h1>
     <div class="welcome">
-        <p><em>Ping the People</em> makes it easy to discover, track, and get instant updates on Indiana legislation that is important to you. Here's how to get started:</p>
+        <p><strong>Ping the People</strong> makes it easy to discover, track, and get instant updates on Indiana legislation that is important to you. Here's how to get started:</p>
         <ol>
             <li>Search <a href="/all-bills"><em>All Legislation</em></a> below by number, committee, subject, keyword, etc. Click the <strong>Watch</strong> button to add legislation to your watchlist. You can add or remove items whenever you'd like.</li>
             <li>Check <watchlist-link><em>My Watchlist</em></watchlist-link> for the latest legislative updates and scheduled events. You can also choose to receive email and/or text message updates for each item.</li>
