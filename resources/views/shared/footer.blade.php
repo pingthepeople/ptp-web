69ad9a614ffff8c3c1ee234a10cbbada0e146f0f
John Hoerr <jhoerr@iu.edu>
2017-04-19 16:56:33 -0400

    Add footer link to `About` page

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,7 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="twelve columns">
-                <p>&copy; 2017 <a href="https://pingthepeople.org">Ping The People</a>. <span style="margin-left:1em;">Questions/Comments/Bugs? <a href="mailto:help@pingthepeople.org">Get in touch!</a></span><p>
+                <p>&copy; 2017 <a href="https://pingthepeople.org">Ping The People</a>. <span style="margin-left:1em;">Questions/Comments/Bugs? <a href="mailto:help@pingthepeople.org">Get in touch!</a></span
+                 | <a href="/about">About</a><p>
             </div>
         </div>
     </div>
