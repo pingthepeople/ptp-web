4c6f2baff7684eb1013a79e4c7930a97ac1d2746
John Hoerr <jhoerr@iu.edu>
2017-04-19 13:52:40 -0400

    Add policy link to footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,7 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="twelve columns">
-                <p>&copy; 2017 <a href="https://pingthepeople.org">Ping The People</a>. <span style="margin-left:1em;">Questions/Comments/Bugs? <a href="mailto:help@pingthepeople.org">Get in touch!</a></span><p>
+                <p>
+                    &copy; 2017 <a href="/">Ping The People</a>. 
+                    <span style="margin-left:1em;">Questions/Comments/Bugs? <a href="mailto:help@pingthepeople.org">Get in touch!</a></span> | 
+                    <a href="/privacy">Privacy Policy</a>
+                <p>
             </div>
         </div>
     </div>
