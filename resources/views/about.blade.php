84749de379fe246bedac9140226475fbbdee2200
John Hoerr <jhoerr@iu.edu>
2017-04-19 16:55:45 -0400

    Add `About` page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.guest')
+
+@section('title', 'About | Ping the People')
+
+@section('content')
+
+<div class="container" >
+    <div class="main">
+    
+        <h1 class="section-title">About</h1>
+
+        <p>
+        Ping the People is a free and <a href="https://github.com/pingthepeople">open source</a> project built with love by two Bloomington citizen-nerds, <a href="https://twitter.com/johnhoerr">John Hoerr</a> and <a href="https://twitter.com/ohnoitsaustin">Austin Lord</a>. We are dedicated to the proposition that it should be easy for Hoosiers to keep tabs on what's happening in the Indiana Statehouse. If you have any questions or comments, please email us at <a href="mailto:help@pingthepeople.org?Subject=Question/comment" target="_top">help@pingthepeople.org</a> or tweet us @pingthepeople.
+        </p>
+
+        <p>
+        Ping the People is not affiliated with any political party or company. While the service is available free of charge, we're happy to accept <a href="/support">donations</a> via PayPal to help cover the cost of running the site. If you want to contribute to the software development effort, please <a href="mailto:help@pingthepeople.org?Subject=Open%20source" target="_top">get in touch</a>!
+        </p>
+        
+    </div>
+</div>
+
+@endsection
