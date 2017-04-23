602b32e7b90513254365a7c7c048b994211456ad
Austin Lord <ahlord@iu.edu>
2017-04-23 14:55:31 -0400

    add header to privacy policy page

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -4,39 +4,38 @@
 
 @section('content')
 
+@include('shared.guest-header')
+
 <div class="container" >
-    <div class="main">
-    
-        <h1 class="section-title">Privacy Policy</h1>
+    <h1 class="section-title">Privacy Policy</h1>
 
-        <p>Your privacy is important to us.</p>
+    <p>Your privacy is important to us.</p>
 
-        <p>Ping the People collects some personal information in order to faciliate timely communication with you regarding the status of legislation. We have outlined the information we collect below. </p>
+    <p>Ping the People collects some personal information in order to faciliate timely communication with you regarding the status of legislation. We have outlined the information we collect below. </p>
 
-        <ul>
-            <li>Your name</li>
-            <li>The email address affiliated with your preferred login method</li>
-            <li>The email address at which you will receive legislative notifications and updates (optional)</li>
-            <li>The mobile phone number which you will receive legislative notifications and updates (optional)</li>
-        </ul>
+    <ul>
+        <li>Your name</li>
+        <li>The email address affiliated with your preferred login method</li>
+        <li>The email address at which you will receive legislative notifications and updates (optional)</li>
+        <li>The mobile phone number which you will receive legislative notifications and updates (optional)</li>
+    </ul>
 
-        <p>It is Ping the People's policy to respect your privacy regarding any information we may collect while operating our website. Accordingly, we have developed this privacy policy in order for you to understand how we collect, use, communicate, disclose and otherwise make use of personal information. We have outlined our privacy policy below.</p>
+    <p>It is Ping the People's policy to respect your privacy regarding any information we may collect while operating our website. Accordingly, we have developed this privacy policy in order for you to understand how we collect, use, communicate, disclose and otherwise make use of personal information. We have outlined our privacy policy below.</p>
 
-        <ul>
-            <li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.</li>
-            <li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
-            <li>We will collect and use personal information solely for fulfilling those purposes specified by us and for other ancillary purposes, unless we obtain the consent of the individual concerned or as required by law.</li>
-            <li>We will not sell your personal information to any thirdy party.</li>
-            <li>Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.</li>
-            <li>We will protect personal information by using reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.</li>
-            <li>We will make readily available to customers information about our policies and practices relating to the management of personal information.</li>
-            <li>We will only retain personal information for as long as necessary for the fulfilment of those purposes.</li>
-        </ul>
+    <ul>
+        <li>We will collect personal information by lawful and fair means and, where appropriate, with the knowledge or consent of the individual concerned.</li>
+        <li>Before or at the time of collecting personal information, we will identify the purposes for which information is being collected.</li>
+        <li>We will collect and use personal information solely for fulfilling those purposes specified by us and for other ancillary purposes, unless we obtain the consent of the individual concerned or as required by law.</li>
+        <li>We will not sell your personal information to any thirdy party.</li>
+        <li>Personal data should be relevant to the purposes for which it is to be used, and, to the extent necessary for those purposes, should be accurate, complete, and up-to-date.</li>
+        <li>We will protect personal information by using reasonable security safeguards against loss or theft, as well as unauthorized access, disclosure, copying, use or modification.</li>
+        <li>We will make readily available to customers information about our policies and practices relating to the management of personal information.</li>
+        <li>We will only retain personal information for as long as necessary for the fulfilment of those purposes.</li>
+    </ul>
 
-        <p>We are committed to conducting this service in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained. Ping the People may change this privacy policy from time to time at Ping the People's sole discretion.</p>
+    <p>We are committed to conducting this service in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained. Ping the People may change this privacy policy from time to time at Ping the People's sole discretion.</p>
 
-        <p>Last updated: 19 April 2017</p>
-    </div>
+    <p>Last updated: 19 April 2017</p>
 </div>
 
 @endsection
