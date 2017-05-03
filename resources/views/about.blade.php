5b647edf74b8c14c20cb1bb996aaf1f9223ab724
John Hoerr <jhoerr@iu.edu>
2017-05-03 07:43:56 -0400

    Add thank yous to About page
    thx friends!

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -18,7 +18,26 @@
         <p>
         Ping the People is not affiliated with any political party or company. While the service is available free of charge, we're happy to accept <a href="/support">donations</a> via PayPal to help cover the cost of running the site. If you want to contribute to the software development effort, please <a href="mailto:help@pingthepeople.org?Subject=Open%20source" target="_top">get in touch</a>!
         </p>
-        
+
+        <hr>
+
+        <small>
+            <p>
+            We would like to thank the following people for their invaluable contributions to the creation of Ping the People. None of this would have been possible without their generous help, support, and patience.
+            </p>	        
+            <p>
+                <em>Concept Development and Beta Testing:</em><br/> 
+                <a href="http://bmclwv.org/">Kate Cruikshank</a>, <a href="http://www.btccbloomington.org/">Georg'ann Cattelona</a>, <a href="http://www.btccbloomington.org/">Allison Zimpfer-Hoerr</a>, <a href="https://twitter.com/emcorturillo">Emily Corturillo</a>, <a href="http://indiana.academia.edu/DanHassoun">Dan Hassoun</a></p>
+            <p>
+                <em>Design:</em><br/> 
+                <a href="https://twitter.com/MadGrds">Madeline Grdina</a>, <a href="http://levimcg.com/">Levi McGranahan</a>, Alan Milner
+            </p>
+            <p>
+                <em>Logo:</em><br/> 
+                <a href="http://levimcg.com/">Levi McGranahan</a>
+            </p>
+        </small>
+
     </div>
 </div>
 
