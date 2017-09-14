5e199043a06a4934d261f9ec50c60a4af0fc7c10
Austin Lord <ahlord@iu.edu>
2017-09-13 20:51:03 -0400

    add default seeder with fake data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\Session::class, 5)->create();
+
+        factory(App\Legislator::class, 20)->create();
+
+        factory(App\Bill::class, 2000)->create()->each(function($bill) {
+            $legislators = App\Legislator::inRandomOrder()->get();
+            // give each bill 1-2 Authors
+            $authors = $legislators->splice(0, rand(1,2));
+            for ($i=0; $i < $authors->count(); $i++) {
+                $bill->authors()->attach($authors[$i]->id, ['BillPosition'=>1]);
+            }
+            // give each bill 2-3 coauthors
+            $coauthors = $legislators->splice(0, rand(2,3));
+            for ($i=0; $i < $coauthors->count(); $i++) {
+                $bill->coauthors()->attach($coauthors[$i]->id, ['BillPosition'=>2]);
+            }
+            // give each bill 4-6 sponsors
+            $coauthors = $legislators->splice(0, rand(4,6));
+            for ($i=0; $i < $coauthors->count(); $i++) {
+                $bill->sponsors()->attach($coauthors[$i]->id, ['BillPosition'=>3]);
+            }
+            // give each bill 4-6 coauthors
+            $coauthors = $legislators->splice(0, rand(4,6));
+            for ($i=0; $i < $coauthors->count(); $i++) {
+                $bill->cosponsors()->attach($coauthors[$i]->id, ['BillPosition'=>4]);
+            }
+        });
     }
 }
