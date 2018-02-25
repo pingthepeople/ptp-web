da563ff5fe27ba4a2ef336087af2759a8bda8370
Austin Lord <ahlord@iu.edu>
2018-02-25 16:25:30 -0500

    make AuthProviderId nullable

--- database/migrations/2018_02_24_033842_add_auth_id_to_users.php
+++ database/migrations/2018_02_24_033842_add_auth_id_to_users.php
@@ -14,7 +14,7 @@ class AddAuthIdToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('AuthProviderId');
+            $table->string('AuthProviderId')->nullable();
         });
     }
 
