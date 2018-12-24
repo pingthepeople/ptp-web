52826f055b1f17e0cd8cf81c98f0758114a69200
John Hoerr <jhoerr@gmail.com>
2018-12-23 22:49:50 -0500

    Try using a .deployment file

--- deploy.cmd
+++ deploy.cmd
@@ -2,5 +2,9 @@
 :: deployment to the Azure App Service.
 :: https://github.com/projectkudu/kudu/wiki/Post-Deployment-Action-Hooks
 
+:: The path to this script must be set in the App Service settings under the key,
+:: SCM_POST_DEPLOYMENT_ACTIONS_PATH. 
+:: I've set this to D:\home\site\wwwroot\post-deploy.cmd.
+
 :: Apply any pending database migrations
 php artisan migrate --force
