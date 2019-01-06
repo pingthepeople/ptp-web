316370bd5bb8b43850d7dc45ab29ca96406c4990
John Hoerr <jhoerr@gmail.com>
2019-01-06 09:20:20 -0500

    "watch list" -> "watchlist"

--- resources/views/all-bills.blade.php
+++ resources/views/all-bills.blade.php
@@ -9,8 +9,8 @@
             <p><strong>Ping the People</strong> makes it easy to discover, track, and get instant updates on Indiana legislation that matters to you. Here's how to get started:</p>
             <ol>
                 <li>Search <a href="/account">all legislation</a> below by bill number, committee, subject, or keyword.</li>
-                <li>Add legislation to your watch list by clicking the <em>Tracking</em> toggle (<img class="welcome__tracking-icon" src="images/toggle.svg" alt="use the track legislation checkbox">).</li>
-                <li>Check <a href="/">my watch list</a> for the latest legislative updates and scheduled events. You can also choose to receive email and/or text message updates for each item.</li>
+                <li>Add legislation to your watchlist by clicking the <em>Tracking</em> toggle (<img class="welcome__tracking-icon" src="images/toggle.svg" alt="use the track legislation checkbox">).</li>
+                <li>Check <a href="/">my watchlist</a> for the latest legislative updates and scheduled events. You can also choose to receive email and/or text message updates for each item.</li>
                 <li>Confirm your email and mobile <a href="/account">settings</a> for instant updates and daily roundups.</li>
             </ol>
         </div>
