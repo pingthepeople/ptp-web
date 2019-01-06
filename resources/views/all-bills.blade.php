45ac9d4820f6eb46570c3a8f7b2af32dc0869b70
John Hoerr <jhoerr@gmail.com>
2019-01-06 09:00:03 -0500

    Tweak welcome verbiage.

--- resources/views/all-bills.blade.php
+++ resources/views/all-bills.blade.php
@@ -6,12 +6,12 @@
     @if($showWelcome)
         <h1>Welcome to <strong>Ping the People</strong>!</h1>
         <div class="welcome">
-            <p>You're not yet tracking any legislation. <strong>Ping the People</strong> makes it easy to discover, track, and get instant updates on Indiana legislation that is important to you. Here's how to get started:</p>
+            <p><strong>Ping the People</strong> makes it easy to discover, track, and get instant updates on Indiana legislation that matters to you. Here's how to get started:</p>
             <ol>
-                <li>Search legislation by number, committee, subject, or keyword.</li>
+                <li>Search legislation by bill number, committee, subject, or keyword.</li>
                 <li>Toggle tracking (<img class="welcome__tracking-icon" src="images/toggle.svg" alt="use the track legislation checkbox">) to add legislation to your watchlist.</li>
                 <li>Check <watchlist-link><strong>My watch list</strong></watchlist-link> for the latest legislative updates and scheduled events. You can also choose to receive email or text message updates for each item.</li>
-                <li>Confirm your email and mobile <a href="/account">contact preferences</a> for updates and daily roundups.</li>
+                <li>Confirm your email and mobile <a href="/account">contact preferences</a> for instant updates and daily roundups.</li>
             </ol>
         </div>
         <hr>
