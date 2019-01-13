b04d6fb9386f7000ae05878d83e33e2b6e9b6c36
Austin Lord <ahlord@iu.edu>
2019-01-13 17:38:52 -0500

    stub out main routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './css/app.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './GuestHomeView';
+import AllLegislation from './AllLegislationView';
+import About from './AboutView';
 
 class App extends Component {
   render() {
@@ -11,13 +14,15 @@ class App extends Component {
         <div className="site__wrapper">
           <Header navList={[
             {text: "Home", url: "/", exact: true},
-            {text: "All legislation", url: "/bills"},
-            {text: "About", url: "/about"},
+            {text: "All legislation", url: "/bills/"},
+            {text: "About", url: "/about/"},
           ]}/>
 
           <div className="site__content content-wrapper">
               <div className="main">
-                  
+                <Route path="/" exact component={Home} />
+                <Route path="/bills/" component={AllLegislation} />
+                <Route path="/about/" component={About} />
               </div>
           </div>
           
